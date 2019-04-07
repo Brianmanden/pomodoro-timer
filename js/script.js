4fc4b817234099b9dccebeea4404f4f1b7929494
Brian Andersen <brianmanden@gmail.com>
2019-04-07 13:25:39 +0200

    Refactoring script.js and index.html

--- js/script.js
+++ js/script.js
@@ -18,36 +18,39 @@
     // Add generic eventlisteners
     document.addEventListener('click', (event) => {
         if(event.target.hasAttribute("data-action")){
+            // Elements used in a task element
             const elem = event.target;
             const action = event.target.dataset["action"];
-            const timerElem = elem.parentElement.querySelector("[data-timer]");
-            const taskTextElem = elem.parentElement.querySelector("[data-taskText]");
-            let timerVal = parseInt(timerElem.dataset.timer);
 
             switch(action){
                 case 'close':
-                    console.log(action, elem);
                     elem.parentNode.remove();
                 break;
 
                 case 'edit':
-                    console.log(action, taskTextElem);
+                    const taskTextElem = elem.parentElement.querySelector("[data-taskText]");
                     taskTextElem.contentEditable = true;
                     taskTextElem.focus();
                 break;
 
-                case 'subtract':
-                    if(timerVal > 0){
-                        timerVal--;
-                        timerElem.innerHTML = timerVal;
-                        timerElem.dataset.timer = timerVal;
+                case 'subtractMin':
+                    let subtractValue = parseInt(elem.parentElement.querySelector("[data-timer]").dataset.timer);
+                    if(subtractValue > 0){
+                        subtractValue--;
+                        elem.parentElement.querySelector("[data-timer]").innerHTML = subtractValue;
+                        elem.parentElement.querySelector("[data-timer]").dataset.timer = subtractValue;
                     }
                 break;
 
-                case 'add':
-                    timerVal++;
-                    timerElem.innerHTML = timerVal;
-                    timerElem.dataset.timer = timerVal;
+                case 'addMin':
+                    let addValue = parseInt(elem.parentElement.querySelector("[data-timer]").dataset.timer);
+                    addValue++;
+                    elem.parentElement.querySelector("[data-timer]").innerHTML = addValue;
+                    elem.parentElement.querySelector("[data-timer]").dataset.timer = addValue;
+                break;
+
+                case 'addTask':
+                    console.log(action);
                 break;
             }
         }else{
